3f9582f201ba985a0f144a4a06849c4a22f49b3b
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-31 14:02:14 +0530

    string and arary length method practice

---
+++ javaScript/1.string.js
@@ -0,0 +1,22 @@
+let country = 'india';
+let state = "delhi";
+let district = `howrah`;
+let place = new String("howrah");
+
+// console.log(place);
+
+
+// Array length
+let num = [87, 77, 95, 81, 74, 95];
+console.log(num.length); // array length
+console.log(num[3]);
+
+let change = num[1] = 999;
+console.log(num);
+
+// array can change array mutable
+// string can not be change string immutable
+
+// string length
+let capital = "de lhi"; //string = length space count
+console.log(capital.length);
