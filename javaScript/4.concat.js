bf1baba01215a9cfc626991e28e288c18c4ecd22
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-31 16:24:28 +0530

    practice concat method

---
+++ javaScript/4.concat.js
@@ -0,0 +1,13 @@
+let city = "mumbai";
+let name = "kamaluddin";
+let school = "Bagnan boys high school";
+
+let conCatStr = city.concat(" ").concat(name, " ", school);
+console.log(conCatStr);
+
+let change = city + " " + name;
+console.log(change);
+
+
+console.log(city.includes("i"));// includes
+console.log(city.indexOf("i")); //index / position 
